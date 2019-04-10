f86dfbe15723af5df5040f9e8a88c5482409a78b
mohammedkadiri <mohammedkadiri72@gmail.com>
2019-04-10 17:54:38 +0100

    Implemented the resistor class

---
+++ src/ie/tudublin/Resistor.java
@@ -0,0 +1,17 @@
+package ie.tudublin;
+
+public class Resistor {
+
+    public int value, ones, tens, hundreds;
+
+    public Resistor(int value) 
+    {
+        this.value = value;
+        this.hundreds = (value / 100);
+        this.tens = (value - (hundreds * 100)) / 10;
+        this.ones  = value - ((hundreds * 100)  + (tens * 10));
+    }
+
+
+    
+}
